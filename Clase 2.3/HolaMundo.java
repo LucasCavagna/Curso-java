914582ea55d362f6f26745ce45215dbb953759b8
LucasCavagna <lucasgastoncavagna@gmail.com>
2025-04-03 08:59:01 -0300

    Clase 2.3 - 2.4
    Clase 2.3 y inicio de la clase 2.4

---
+++ Clase 2.3/HolaMundo.java
@@ -0,0 +1,16 @@
+
+//
+// Comentarios de linea
+//
+
+class HolaMundo{
+
+    // Metodo de Hola Mundo
+    public static void main(String[] args){
+
+        System.out.println("Hola Mundo!");
+
+    }
+
+
+}
