b7e903c01fec3f775dfbf28792f41f7807a4143f
LucasCavagna <lucasgastoncavagna@gmail.com>
2025-04-03 09:40:33 -0300

    Corrección
    Se corrigió la clase 2.3 y 2.4 que estaban invertidas

--- Clase 2.3/HolaMundo.java
+++ Clase 2.3/HolaMundo.java
@@ -7,14 +7,8 @@ class HolaMundo{
 
     // Metodo de Hola Mundo
     public static void main(String[] args){
-        
-        int resultado = 20;
 
-        int n = 60;
-        
-        System.out.println("Resultado= " + resultado + 5);
-
-        System.out.println("Resultado= " + (n*5));
+        System.out.println("Hola Mundo!");
 
     }
 
--- Clase 2.4/HolaMundo.java
+++ Clase 2.4/HolaMundo.java
@@ -7,8 +7,14 @@ class HolaMundo{
 
     // Metodo de Hola Mundo
     public static void main(String[] args){
+        
+        int resultado = 20;
 
-        System.out.println("Hola Mundo!");
+        int n = 60;
+        
+        System.out.println("Resultado= " + resultado + 5);
+
+        System.out.println("Resultado= " + (n*5));
 
     }
 
