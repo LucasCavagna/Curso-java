0aaad49a72f5d67694c703884f4df5840bfb143d
LucasCavagna <lucasgastoncavagna@gmail.com>
2025-04-04 14:47:55 -0300

    Clase 2.11 y 2.12

---
+++ Unidad 2/Clase 2.12/Escapes.java
@@ -0,0 +1,13 @@
+public class Escapes{
+
+    // Metodo de Hola Mundo
+    public static void main(String[] args){
+
+        String cadena = "\t \\\'Se tabulo\'\\\rdato";
+
+        System.out.println(cadena);
+
+    }
+
+
+}
