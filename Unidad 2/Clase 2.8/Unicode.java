0205a23bb53494c97757b79212ee28d966a1d260
LucasCavagna <lucasgastoncavagna@gmail.com>
2025-04-04 10:38:20 -0300

    Clase 2.8 y 2.9

---
+++ Unidad 2/Clase 2.8/Unicode.java
@@ -0,0 +1,16 @@
+public class Unicode{
+
+    // Metodo de Hola Mundo
+    public static void main(String[] args){
+
+        char letra1 = '\u0045';
+
+        System.out.println(letra1);
+
+        char letra2 = '\u0046';
+
+        System.out.println(letra2);
+    }
+
+
+}
