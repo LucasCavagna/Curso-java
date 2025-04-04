3fc009913ffc2078719913b48331fc80f4ce2de6
LucasCavagna <lucasgastoncavagna@gmail.com>
2025-04-04 14:57:20 -0300

    Clase 2.13

---
+++ Unidad 2/clase 2.13/Comentarios.java
@@ -0,0 +1,28 @@
+public class Comentarios{
+
+    // Metodo de Hola Mundo
+    public static void main(String[] args){
+
+        // Comentarios de linea
+        
+        
+        /* 
+            Comentarios de bloque 
+
+            tada
+         
+        */
+
+
+        /** 
+         * Comentario para documentacion de java
+         * 
+         * 
+         * Lo utiliza javadoc
+         * 
+         * 
+        */
+    }
+
+
+}
---
+++ Unidad 2/clase 2.13/Constantes.java
@@ -0,0 +1,13 @@
+public class Constantes{
+
+    // Metodo de Hola Mundo
+    public static void main(String[] args){
+
+        final String CADENA = "hola"; // Constante
+
+        System.err.println(CADENA);    
+    
+    }
+
+
+}
