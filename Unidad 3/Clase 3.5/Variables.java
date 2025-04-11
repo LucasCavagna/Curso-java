335c2b10e09c890791b641736575ed872d22cae5
LucasCavagna <lucasgastoncavagna@gmail.com>
2025-04-11 08:24:25 -0300

    Clase 3.4 y 3.5

---
+++ Unidad 3/Clase 3.5/Variables.java
@@ -0,0 +1,27 @@
+public class Variables {
+
+
+    static int edad = 40; // Variable entera
+    public static void main(String[] args) {
+        // Definición de variables
+        int edad = 20; // Variable entera
+        // Imprimir las variables en la consola
+        System.out.println("Edad: " + edad);
+        primero(); // Llamada al método primero
+        segundo(); // Llamada al método segundo
+    }
+
+    static void primero() {
+        // Definición de variables
+        // Imprimir las variables en la consola
+        System.out.println("Edad: " + edad);
+    }
+
+    static void segundo() {
+        // Definición de variables
+        int edad = 25; // Variable entera
+        // Imprimir las variables en la consola
+        System.out.println("Edad: " + edad);
+    }
+
+}
