4724feacac0e3fea396adc51ec7e69159e4f6282
LucasCavagna <lucasgastoncavagna@gmail.com>
2025-06-28 14:52:41 -0300

    Alarming save

---
+++ Unidad 8/Clase 8.15/EjemploMySQL_Update.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class EjemploMySQL_Update {
+
+    public static void main(String[] args) {
+
+        try {
+
+            // realizamos conexión
+            Connection conex = ConexionBD.conectar("agenda");
+
+            // insertamos contacto
+            String query = "UPDATE contacto SET edad = ? WHERE edad = ?";
+            PreparedStatement queryUpdate = conex.prepareStatement(query);
+            queryUpdate.setInt(1, 30);
+            queryUpdate.setInt(2, 20);
+            int resultado = queryUpdate.executeUpdate();
+
+            // analizamos el resultado del insert
+            if (resultado > 0)
+                System.out.println("Se han modificado " + resultado + " contactos correctamente");
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ Unidad 8/Clase 8.16/EjemploMySQL_Delete.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class EjemploMySQL_Delete {
+
+    public static void main(String[] args) {
+        
+            try {
+    
+                // realizamos conexión
+                Connection conex = ConexionBD.conectar("agenda");
+    
+                // insertamos contacto
+                String query = "DELETE FROM contacto WHERE edad = ?";
+                PreparedStatement queryDelete = conex.prepareStatement(query);
+                queryDelete.setInt(1, 30);
+                int resultado = queryDelete.executeUpdate();
+    
+                // analizamos el resultado del insert
+                if (resultado > 0)
+                    System.out.println("Se han borrado " + resultado + " contactos correctamente");
+    
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        
+
+    }
+}
